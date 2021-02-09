92c78713ad1e14522ab00e41a8056226741f838a
Lucas Barrionuevo <lukitaz_lbdp@hotmail.com>
2021-02-09 14:09:54 -0300

    cambios de imgs

--- src/Otros/Carrusel.js
+++ src/Otros/Carrusel.js
@@ -7,21 +7,21 @@ const Nosotros = () => {
             <Carousel className="pt-5">
                 <Carousel.Item interval={4000}>
                     <img
-                        className="d-block w-100"
+                        className="d-block w-80"
                         src={process.env.PUBLIC_URL + "/slider1.png"}
                         alt="First slide" height="550px"
                     />
                 </Carousel.Item>
                 <Carousel.Item interval={4000}>
                     <img
-                        className="d-block w-100"
+                        className="d-block w-80"
                         src={process.env.PUBLIC_URL + "/slider1.png"}
                         alt="Second slide" height="550px"
                     />
                 </Carousel.Item>
                 <Carousel.Item interval={4000}>
                     <img
-                        className="d-block w-100"
+                        className="d-block w-80"
                         src={process.env.PUBLIC_URL + "/slider1.png"}
                         alt="Third slide" height="550px"
                     />
--- src/Otros/Jumbo.js
+++ src/Otros/Jumbo.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Jumbotron from "react-bootstrap/Jumbotron";
-import "../App.css"
+import "../App.css";
 
 const Jumbo = () => {
-    return (
-        <Jumbotron className="mt-5 text-center aqua-gradient" style={{ width: "100%" }}>
 
-            <h2 style={{ textTransform: "initial"}} espacio>Nosotros</h2>
+    return (
+        <Jumbotron className="mt-5 text-center shadow-sm rounded" style={{backgroundImage:"url(./back.png)", width:"100%"}}>
+
+            <h2 style={{ textTransform: "initial"}}>Nosotros</h2>
             <p>Somos una empresa tucumana que llega para brindar la mejor calidad en comunicación a través de un servicio automático y personalizado a la vez, dejando resultados reales y eficaces.</p>
 
         </Jumbotron>
