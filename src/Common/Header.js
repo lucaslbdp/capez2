cc62fe9b7e246490d09bd12832830e6fd9ddfa0b
Lucas Barrionuevo <lukitaz_lbdp@hotmail.com>
2021-02-09 21:03:25 -0300

    boceto terminado 2

--- src/Common/Header.js
+++ src/Common/Header.js
@@ -12,17 +12,17 @@ const Header = () => {
                     target="_blank noreferrer"
                     href="https://www.instagram.com/capezcomunicacion/"
                 >
-                    <img src={process.env.PUBLIC_URL + "/instagramIcon.png"} className="mr-4" style={{maxWidth:"50px"}}></img></a>
+                    <img src={process.env.PUBLIC_URL + "/instagramIcon.png"} className="mr-4" style={{maxWidth:"40px"}}></img></a>
                 <a style={{ color: "black" }}
                     target="_blank noreferrer"
                     href="https://api.whatsapp.com/send?phone=+5493815516016"
                 >
-                    <img src={process.env.PUBLIC_URL + "/whatsappIcon.png"} style={{maxWidth:"50px"}} className="mr-4"></img></a>
+                    <img src={process.env.PUBLIC_URL + "/whatsappIcon.png"} style={{maxWidth:"40px"}} className="mr-4"></img></a>
                 <a style={{ color: "black" }}
                     target="_blank noreferrer"
                     href="https://www.facebook.com/capezcomunicacion"
                 >
-                    <img src={process.env.PUBLIC_URL + "/facebookIcon.png"} style={{maxWidth:"50px"}} className="mr-4"></img></a>
+                    <img src={process.env.PUBLIC_URL + "/facebookIcon.png"} style={{maxWidth:"40px"}} className="mr-4"></img></a>
             </Navbar.Brand>
         </MDBNavbar>
     );
--- src/Otros/Jumbo.js
+++ src/Otros/Jumbo.js
@@ -5,10 +5,10 @@ import "../App.css";
 const Jumbo = () => {
 
     return (
-        <Jumbotron className="mt-5 text-center text-light shadow-sm mb-0" style={{backgroundImage:"url(./back.png)", width:"100%"}}>
+        <Jumbotron className="mt-5 text-center shadow-sm mb-0" style={{backgroundImage:"url(./back.png)", width:"100%"}}>
             <img src={process.env.PUBLIC_URL + "/capezGris.png"} className="mb-0 mt-0"></img>
-            <h2 style={{ textTransform: "initial", letterSpacing:"0cm", fontSize:"45px"}} className="text-light"><strong>Nosotros</strong></h2>
-            <p style={{fontSize:"22px"}}>Somos una empresa tucumana que llega para brindar la mejor calidad en comunicación a través de un servicio automático y personalizado a la vez, dejando resultados reales y eficaces.</p>
+            <h2 style={{ textTransform: "initial", letterSpacing:"0cm", fontSize:"45px", color:"white"}} className="mb-3"><strong>Nosotros</strong></h2>
+            <p style={{fontSize:"22px", color:"white"}} className="mb-4">Somos una empresa tucumana que llega para brindar la mejor calidad en comunicación a través de un servicio automático y personalizado a la vez, dejando resultados reales y eficaces.</p>
 
         </Jumbotron>
 
