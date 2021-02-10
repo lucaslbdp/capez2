560d8717fe79906d7bee207793ff5b56b28b10cb
Lucas Barrionuevo <lukitaz_lbdp@hotmail.com>
2021-02-10 10:27:43 -0300

    rt

--- src/Otros/Jumbo.js
+++ src/Otros/Jumbo.js
@@ -5,7 +5,7 @@ import "../App.css";
 const Jumbo = () => {
 
     return (
-        <Jumbotron className="mt-5 text-center shadow-sm mb-0" style={{backgroundImage:"url(./back.png)", width:"100%"}}>
+        <Jumbotron className="mt-5 text-center shadow-sm mb-0 pt-0" style={{backgroundImage:"url(./back.png)", width:"100%"}}>
             <img src={process.env.PUBLIC_URL + "/capezGris.png"} className="pb-0 mb-0"></img>
             <h2 style={{ textTransform: "initial", letterSpacing:"0cm", fontSize:"45px", color:"white"}} className="mb-3"><strong>Nosotros</strong></h2>
             <p style={{fontSize:"22px", color:"white"}} className="mb-4">Somos una empresa tucumana que llega para brindar <br></br> la mejor calidad en comunicación a través de un servicio <br></br> automático y personalizado a la vez, dejando resultados <br></br>reales y eficaces.</p>
